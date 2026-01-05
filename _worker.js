57f809b42d034b22d8e0767f27d344eacf0ac92a
tangfeiran <fayong_zhou@126.com>
2026-01-05 10:01:28 +0800

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- _worker.js
+++ _worker.js
@@ -18,7 +18,8 @@ https://cfxr.eu.org/getSub
 
 let urls = [];
 let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
+// let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
+let subConfig = "https://raw.githubusercontent.com/tangfeiran/CF-Workers-SUB/refs/heads/main/ACL4SSR_Online_MultiCountry.ini"
 let subProtocol = 'https';
 
 export default {
@@ -825,4 +826,5 @@ async function KV(request, env, txt = 'ADD.txt', guest) {
 			headers: { "Content-Type": "text/plain;charset=utf-8" }
 		});
 	}
+
 }
