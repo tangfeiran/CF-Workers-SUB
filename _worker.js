3498bcc25b6935dc86d0728ce04e036f4f3b11a5
tangfeiran <fayong_zhou@126.com>
2026-01-07 10:33:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -19,7 +19,8 @@ https://cfxr.eu.org/getSub
 let urls = [];
 let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 // let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
-let subConfig = "https://raw.githubusercontent.com/tangfeiran/CF-Workers-SUB/refs/heads/main/ACL4SSR_Online_MultiCountry.ini"
+// let subConfig = "https://raw.githubusercontent.com/tangfeiran/CF-Workers-SUB/refs/heads/main/ACL4SSR_Online_MultiCountry.ini"
+let subConfig = "https://gist.githubusercontent.com/vernesong/4c27ed54ab2a5fedd9c4011389ac11ed/raw/lhie1_dler.ini"
 let subProtocol = 'https';
 
 export default {
@@ -828,3 +829,4 @@ async function KV(request, env, txt = 'ADD.txt', guest) {
 	}
 
 }
+
